a2e6fbc3d0ac5b562c03348676074af540979f82
ptsfdtz <pitousanfadetuzi@gmail.com>
2024-09-15 18:03:59 +0800

    update cat-paw-keyboard

--- server/cat-paw-keyboard/cat-paw-keyboard.ino
+++ server/cat-paw-keyboard/cat-paw-keyboard.ino
@@ -1,12 +1,4 @@
-#include <BleKeyboard.h>
-
-BleKeyboard bleKeyboard("ESP32 Keyboard", "ESP32", 100);
-
-#define BUTTON_PIN_1 4
-#define BUTTON_PIN_2 5
-#define BUTTON_PIN_3 12
-#define BUTTON_PIN_4 13
-#define BUTTON_PIN_5 14
+#include "header.h"
 
 void setup() {
   Serial.begin(9600);
@@ -17,7 +9,7 @@ void setup() {
   pinMode(BUTTON_PIN_4, INPUT_PULLUP);
   pinMode(BUTTON_PIN_5, INPUT_PULLUP);
 
-    bleKeyboard.begin();
+  bleKeyboard.begin();
   Serial.println("BLE Keyboard started");
 }
 
@@ -25,49 +17,25 @@ void loop() {
   if (bleKeyboard.isConnected()) {
     Serial.println("BLE connected");
 
-    if (digitalRead(BUTTON_PIN_1) == LOW) {
-      Serial.println("Button 1 pressed");
-      bleKeyboard.press('z');
-      Serial.println("Sent: z");
-      delay(100);
-      bleKeyboard.releaseAll();
-    }
-
-    if (digitalRead(BUTTON_PIN_2) == LOW) {
-      Serial.println("Button 2 pressed");
-      bleKeyboard.press('x');
-      Serial.println("Sent: x");
-      delay(100);
-      bleKeyboard.releaseAll();
-    }
-
-    if (digitalRead(BUTTON_PIN_3) == LOW) {
-      Serial.println("Button 3 pressed");
-      bleKeyboard.press('c');
-      Serial.println("Sent: c");
-      delay(100);
-      bleKeyboard.releaseAll();
-    }
-
-    if (digitalRead(BUTTON_PIN_4) == LOW) {
-      Serial.println("Button 4 pressed");
-      bleKeyboard.press('v');
-      Serial.println("Sent: v");
-      delay(100);zzzz
-      bleKeyboard.releaseAll();
-    }
-
-    if (digitalRead(BUTTON_PIN_5) == LOW) {
-      Serial.println("Button 5 pressed");
-      bleKeyboard.press(KEY_LEFT_CTRL);
-      Serial.println("Sent: KEY_LEFT_CTRL");
-      delay(100);   
-       }
-
-  } 
-  else {
+    checkContinuousPress(BUTTON_PIN_1, 'z');
+    checkContinuousPress(BUTTON_PIN_2, 'x');
+    checkContinuousPress(BUTTON_PIN_3, 'c');
+    checkContinuousPress(BUTTON_PIN_4, 'v');
+    checkContinuousPress(BUTTON_PIN_5, KEY_LEFT_CTRL); 
+    
+  } else {
     Serial.println("BLE not connected");
   }
 
   delay(10);
+}
+
+void checkContinuousPress(int pin, char key) {
+  if (digitalRead(pin) == LOW) {
+    bleKeyboard.press(key);
+    Serial.printf("Sent: %c\n", key);
+    delay(100); 
+  } else {
+    bleKeyboard.release(key);
+  }
 }
---
+++ server/cat-paw-keyboard/header.h
@@ -0,0 +1,9 @@
+#include <BleKeyboard.h>
+
+#define BUTTON_PIN_1 4
+#define BUTTON_PIN_2 5
+#define BUTTON_PIN_3 12
+#define BUTTON_PIN_4 13
+#define BUTTON_PIN_5 14
+
+BleKeyboard bleKeyboard("ESP32 Keyboard", "ESP32");
